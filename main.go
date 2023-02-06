4d2773fb2c5f85d77f512544f2c7f3e533ac9d63
billy <aphexlog@gmail.com>
2023-02-06 12:25:59 -0600

    creating aws env vars and outputing them to shell

--- main.go
+++ main.go
@@ -31,10 +31,30 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf(
-		"Account: %s\nUserID: %s\nARN: %s\n",
-		aws.ToString(identity.Account),
-		aws.ToString(identity.UserId),
-		aws.ToString(identity.Arn),
-	)
+	// create environment variables
+	os.Setenv("AWS_ACCOUNT_ID", aws.ToString(identity.Account))
+	os.Setenv("AWS_USER_ID", aws.ToString(identity.UserId))
+	os.Setenv("AWS_ARN", aws.ToString(identity.Arn))
+
+	fmt.Println("AWS_ACCOUNT_ID:", os.Getenv("AWS_ACCOUNT_ID"))
+	fmt.Println("AWS_USER_ID:", os.Getenv("AWS_USER_ID"))
+	fmt.Println("AWS_ARN:", os.Getenv("AWS_ARN"))
+
+	// get the credentials
+	creds, err := cfg.Credentials.Retrieve(context.TODO())
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+
+	// create environment variables
+	os.Setenv("AWS_ACCESS_KEY_ID", creds.AccessKeyID)
+	os.Setenv("AWS_SECRET_ACCESS_KEY", creds.SecretAccessKey)
+	os.Setenv("AWS_SESSION_TOKEN", creds.SessionToken)
+
+	// print the credentials
+	fmt.Println("AWS_ACCESS_KEY_ID:", os.Getenv("AWS_ACCESS_KEY_ID"))
+	fmt.Print("AWS_SECRET_ACCESS_KEY:", os.Getenv("AWS_SECRET_ACCESS_KEY"))
+	fmt.Print("AWS_SESSION_TOKEN:", os.Getenv("AWS_SESSION_TOKEN"))
+
 }
