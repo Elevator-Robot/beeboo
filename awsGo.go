c0136581ae35334c362d7a893bb56674384fd330
billy <aphexlog@gmail.com>
2023-02-07 13:20:41 -0600

    adding cobra cli framework & a little description

---
+++ awsGo.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+)
+
+func awsGo() {
+	cfg, err := config.LoadDefaultConfig(
+		context.TODO(),
+		config.WithSharedConfigProfile("aphexlog"),
+	)
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+
+	client := sts.NewFromConfig(cfg)
+
+	identity, err := client.GetCallerIdentity(
+		context.TODO(),
+		&sts.GetCallerIdentityInput{},
+	)
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+
+	// create environment variables
+	os.Setenv("AWS_ACCOUNT_ID", aws.ToString(identity.Account))
+	os.Setenv("AWS_USER_ID", aws.ToString(identity.UserId))
+	os.Setenv("AWS_ARN", aws.ToString(identity.Arn))
+
+	fmt.Println("AWS_ACCOUNT_ID:", os.Getenv("AWS_ACCOUNT_ID"))
+	fmt.Println("AWS_USER_ID:", os.Getenv("AWS_USER_ID"))
+	fmt.Println("AWS_ARN:", os.Getenv("AWS_ARN"))
+
+	// get the credentials
+	creds, err := cfg.Credentials.Retrieve(context.TODO())
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+
+	// create environment variables
+	os.Setenv("AWS_ACCESS_KEY_ID", creds.AccessKeyID)
+	os.Setenv("AWS_SECRET_ACCESS_KEY", creds.SecretAccessKey)
+	os.Setenv("AWS_SESSION_TOKEN", creds.SessionToken)
+
+	// print the credentials
+	fmt.Println("AWS_ACCESS_KEY_ID:", os.Getenv("AWS_ACCESS_KEY_ID"))
+	fmt.Print("AWS_SECRET_ACCESS_KEY:", os.Getenv("AWS_SECRET_ACCESS_KEY"))
+	fmt.Print("AWS_SESSION_TOKEN:", os.Getenv("AWS_SESSION_TOKEN"))
+
+}
--- main.go
+++ main.go
@@ -1,60 +1,18 @@
+/*
+Copyright © 2023 Elevator Robot
+
+Meet BeeBoop, the CLI with a BZZZ-tastic personality!
+This little guy may be small in size, but he's got big aspirations.
+BeeBoop dreams of one day transforming from a simple code machine into a real live human boy.
+Can you imagine a CLI with a love for honey, flowers, and adventure?
+It's like a scene straight out of a Disney movie! With his determination and can-do spirit, BeeBoop is on a mission to make his wildest dreams come true.
+So buzz on over and join in on the fun with BeeBoop, the CLI with a heart of honey and a smile as sweet as nectar. Just dont let'm use knives :)
+
+*/
 package main
 
-import (
-	"context"
-	"fmt"
-	"os"
-
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/sts"
-)
+import "github.com/elevator-robot/hal/beeboop/cmd"
 
 func main() {
-	cfg, err := config.LoadDefaultConfig(
-		context.TODO(),
-		config.WithSharedConfigProfile("aphexlog"),
-	)
-	if err != nil {
-		fmt.Println("error:", err)
-		os.Exit(1)
-	}
-
-	client := sts.NewFromConfig(cfg)
-
-	identity, err := client.GetCallerIdentity(
-		context.TODO(),
-		&sts.GetCallerIdentityInput{},
-	)
-	if err != nil {
-		fmt.Println("error:", err)
-		os.Exit(1)
-	}
-
-	// create environment variables
-	os.Setenv("AWS_ACCOUNT_ID", aws.ToString(identity.Account))
-	os.Setenv("AWS_USER_ID", aws.ToString(identity.UserId))
-	os.Setenv("AWS_ARN", aws.ToString(identity.Arn))
-
-	fmt.Println("AWS_ACCOUNT_ID:", os.Getenv("AWS_ACCOUNT_ID"))
-	fmt.Println("AWS_USER_ID:", os.Getenv("AWS_USER_ID"))
-	fmt.Println("AWS_ARN:", os.Getenv("AWS_ARN"))
-
-	// get the credentials
-	creds, err := cfg.Credentials.Retrieve(context.TODO())
-	if err != nil {
-		fmt.Println("error:", err)
-		os.Exit(1)
-	}
-
-	// create environment variables
-	os.Setenv("AWS_ACCESS_KEY_ID", creds.AccessKeyID)
-	os.Setenv("AWS_SECRET_ACCESS_KEY", creds.SecretAccessKey)
-	os.Setenv("AWS_SESSION_TOKEN", creds.SessionToken)
-
-	// print the credentials
-	fmt.Println("AWS_ACCESS_KEY_ID:", os.Getenv("AWS_ACCESS_KEY_ID"))
-	fmt.Print("AWS_SECRET_ACCESS_KEY:", os.Getenv("AWS_SECRET_ACCESS_KEY"))
-	fmt.Print("AWS_SESSION_TOKEN:", os.Getenv("AWS_SESSION_TOKEN"))
-
+	cmd.Execute()
 }
